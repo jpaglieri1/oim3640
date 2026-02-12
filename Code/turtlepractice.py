eb6e50fd78a5e6bcb3e95b2d226fb9167b42d2ac
jpaglieri1 <jpaglieri1@babson.edu>
2026-02-12 11:14:52 -0500

    Session7Work
    Here is my work from session 7, as well as my learning logs from sessions 6 and 7.

---
+++ Code/turtlepractice.py
@@ -0,0 +1,27 @@
+import turtle
+
+
+def draw_square(turtle_obj, size = 100):
+    """Draw a square wit hthe given length of side"""
+    for _ in range(4):
+        turtle_obj.forward(size)
+        turtle_obj.left(90)
+
+
+def draw_spiral(t):
+    """day one square, turn a angle, draw another square, and so on"""
+
+    for i in range(72):
+        draw_square(t, 50 + i*2)
+        t.left(5)
+
+def main():
+    t = turtle.Turtle()
+    t.speed(0)
+    draw_square(t)
+    #draw_square(t, size = 50)
+    #draw_spiral(t)
+    turtle.mainloop()
+
+if __name__ == "__main__":
+    main()
