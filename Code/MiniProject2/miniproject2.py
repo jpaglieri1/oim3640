4745a0bbbfa0f4049d2276440210f79323e1cae6
jpaglieri1 <jpaglieri1@babson.edu>
2026-04-06 13:32:59 -0400

    Update miniproject2.py
    I created the function dataclean, using ChatGPT to identify text(i.e. prepositions, spaces) that should be removed as well as some things to consider to ensure data that I don't want to be removed doesn't get removed.

--- Code/MiniProject2/miniproject2.py
+++ Code/MiniProject2/miniproject2.py
@@ -2,4 +2,18 @@ def resupload():
     resume = open(input("Input directory to file: "))
     return(resume)
 
-resupload()
+def jobdesc():
+    jd = open(input("Input directory to job description file: "))
+    return(jd)
+
+def dataclean(file):
+    prepositions = ["in", "the", "it", "of", "a", "to", "with", "to", "for", "on", "at", "by", "from",
+                    "into", "about", "between", "like", "after", "over", "through"]
+    file = file.lower()
+    for word in prepositions:
+        for i in file:
+            if word == i:
+                file = file.remove(i)
+    for " " in file:
+        file = file.remove(" ")
+    return file
